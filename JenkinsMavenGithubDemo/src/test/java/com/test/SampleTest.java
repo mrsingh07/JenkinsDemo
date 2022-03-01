53a854fa9f543357df07d46f3e31c5a7c17c69a9
Harvinder Singh <harvinder07192@gmail.com>
2022-03-01 19:52:46 +0530

    First Commit

---
+++ JenkinsMavenGithubDemo/src/test/java/com/test/SampleTest.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+import com.base.BaseClass;
+
+public class SampleTest extends BaseClass{
+	
+	@Test
+	public void test1()
+	{
+		System.out.println("Execute TestCase 1");
+	}
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("Execute TestCase 2");
+	}
+	
+	@Test
+	public void test3()
+	{
+		System.out.println("Execute TestCase 3");
+	}
+	
+
+}
